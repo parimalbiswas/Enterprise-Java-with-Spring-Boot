94e67a29c1610f978e6b6d5d1596327356e91eea
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-08-20 10:43:57 +0530

    this key words

---
+++ 20th August 2022/First/src/LaunchEncap4This.java
@@ -0,0 +1,37 @@
+class Ali
+{
+	private int age;
+	private String name;
+//	windows shift I -> Setter getter shortCart
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
+
+public class LaunchEncap4This
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
