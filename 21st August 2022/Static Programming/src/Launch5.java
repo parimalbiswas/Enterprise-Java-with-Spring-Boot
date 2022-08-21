77994f698992370b8324147deeae0ea02daa2d85
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-08-21 11:41:34 +0530

    .

--- 21st August 2022/Static Programming/src/Launch5.java
+++ 21st August 2022/Static Programming/src/Launch5.java
@@ -1,8 +1,35 @@
+class Jio {
+	private int age;
+	private String name;
+
+	static int count;
+
+	Jio() {
+		count++;
+	}
+
+	Jio(int age) {
+		this.age = age;
+		count++;
+	}
+
+	Jio(int age, String name) {
+		this.age = age;
+		this.name = name;
+		count++;
+	}
+
+}
 
 public class Launch5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Jio j1 = new Jio();
+		Jio j2 = new Jio(20);
+		Jio j3 = new Jio(20, "jajhjkdhfdh");
+
+		System.out.println(Jio.count);
 
 	}
 
---
+++ 21st August 2022/Static Programming/src/Launch6.java
@@ -0,0 +1,15 @@
+
+public class Launch6 {
+	static int b;
+	static {
+		System.out.println(a); // Error
+		System.out.println(b);
+	}
+	static int a; // HAve to write before
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
