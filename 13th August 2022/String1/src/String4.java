3189ff7906d0d22e6f0297f750c77f23748bcff2
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-08-22 10:47:22 +0530

    .

--- 13th August 2022/String1/src/String4.java
+++ 13th August 2022/String1/src/String4.java
@@ -4,8 +4,25 @@ public class String4
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-2:11
+
+		String s1 = "Google"; // memory allocated in String C Pool ( Because Its not made using new Keyword )
+		String s2 = "Java"; // memory allocated in String C Pool ( Because Its not made using new Keyword )
+		String s3 = "Google" + "Java"; // memory allocated in String C Pool ( Because Its not made using new Keyword )
+
+		String s4 = s1 + s2; // If reference are involved right side. that will not resolved during compile
+								// time. That will be resolved during run time || Thats why object will be
+								// create in Heap Area not in SCP area
+
+		String s5 = s1 + "Khoka"; // At least one above reference variable on the right side. so object will be
+									// create in Heap Area not in SCP area
+
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(s2));
+		System.out.println(s1.compareTo(s2)); // compareTo return integer value. if both char are equal it return 0.
+		// Its match with ASCII value
+
 	}
 
 }
