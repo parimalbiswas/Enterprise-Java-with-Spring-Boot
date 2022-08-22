8f52a18fb05715341568e0c5c30f8d8660966514
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-08-22 12:39:21 +0530

    Update Launch7.java

--- 13th August 2022/String1/src/Launch7.java
+++ 13th August 2022/String1/src/Launch7.java
@@ -4,7 +4,21 @@ public class Launch7
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+
+		String s1 = "Jio" + "JAVA"; // JioJAVA
+		System.out.println(s1);
+
+		String s2 = "Jio" + 99; // Jio99
+		System.out.println(s2);
+
+		String s3 = "Jio" + 99 + 99 + 10; // Jio999910
+		System.out.println(s3);
+
+		String s4 = 10 + 14 + "Jio"; // 24Jio
+		System.out.println(s4);
+
+		String s5 = 10 + 14 + 2; // Error Mandatory when trying to concat .otherwise it will give error
+		System.out.println(s5);
 
 	}
 
