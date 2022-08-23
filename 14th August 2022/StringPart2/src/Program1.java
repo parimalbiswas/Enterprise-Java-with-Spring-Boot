cc30fd797399ee19e29597f7ac11ae3a44ecaf20
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-08-23 19:39:27 +0530

    .

---
+++ 14th August 2022/StringPart2/src/Program1.java
@@ -0,0 +1,20 @@
+class Alien
+{
+
+}
+
+public class Program1
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String s1 = new String();
+		if (true)
+		{
+
+		}
+
+	}
+
+}
