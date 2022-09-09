e2bf80fdd7105e545e04e4400d271e6e47ed04f2
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-09-09 21:46:25 +0530

    .

---
+++ 31st_July_22/Array/src/Array1.java
@@ -0,0 +1,32 @@
+//What is Array ? --> Array is Index based Data Structure to store Large number of Homogeneous (Same type) data in a Single Variable
+
+//Array is treated is Object . So use new Keyword;
+// int a [] = new int[10]  --> 10 memory will create in array format.
+
+
+
+//Array is two type 
+//1. Regular Array
+//2. Jacked Array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class Array1 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
--- 31st_July_22/Array/src/P1.java
+++ 31st_July_22/Array/src/P1.java
@@ -1,8 +1,25 @@
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class P1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int [] a = new int[5];
+//		a[0] = 10;
+//		a[1] = 20;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("Please enter the marks"+i);
+			a[i]=scanner.nextInt();
+		}
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i] +" "+i);
+		}
+		
+		
 
 	}
 
