b52f916a87c5106d702d55d976859530b987d9e2
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-09-17 12:33:21 +0530

    .

--- 17th September 2022/Live/src/Launch5.java
+++ 17th September 2022/Live/src/Launch5.java
@@ -1,11 +1,32 @@
+
 //partiaclaly - bi=oth checked and unchaked exception present.\
+
+// finally to close the resourse.
+
+class Demo10
+{
+	void display() throws ArithmeticException
+	{
+		System.out.println("Parent Class overridden method");
+	}
+}
+
+class Demo12 extends Demo10
+{
+	@Override
+	void display()
+	{
+		System.out.println("overriddening in child");
+	}
+}
 
 public class Launch5
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		Demo12 d12 = new Demo12();
+		d12.display();
 
 	}
 
