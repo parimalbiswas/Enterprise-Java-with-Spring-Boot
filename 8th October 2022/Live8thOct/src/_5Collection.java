615217b32eb2c973e73586305e5bd044861bc1cc
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-10-09 12:33:06 +0530

    .

--- 8th October 2022/Live8thOct/src/_5Collection.java
+++ 8th October 2022/Live8thOct/src/_5Collection.java
@@ -5,13 +5,14 @@ public class _5Collection
 
 	public static void main(String[] args)
 	{
-		PriorityQueue pq = new PriorityQueue<>();
+		PriorityQueue pq = new PriorityQueue();
 		pq.add(100);
 		pq.add(50);
 		pq.add(150);
 		pq.add(25);
 		pq.add(125);
 		pq.add(175);
+		pq.add(25);
 
 		System.out.println(pq);
 
---
+++ 8th October 2022/Live8thOct/src/_6Collection.java
@@ -0,0 +1,24 @@
+import java.util.TreeSet;
+
+public class _6Collection
+{
+
+	public static void main(String[] args)
+	{
+		// Sorted Order in TreeSet
+		// Duplicate Not Allowed
+		TreeSet ts = new TreeSet<>();
+		ts.add(100);
+		ts.add(50);
+		ts.add(150);
+		ts.add(25);
+		ts.add(75);
+		ts.add(125);
+		ts.add(175);
+		ts.add(150);
+
+		System.out.println(ts);
+
+	}
+
+}
