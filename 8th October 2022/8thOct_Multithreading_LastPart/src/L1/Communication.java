0e6523d0999a553e47e530b18084b45566e272d8
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-10-14 01:24:11 +0530

    .

---
+++ 8th October 2022/8thOct_Multithreading_LastPart/src/L1/Communication.java
@@ -0,0 +1,72 @@
+package L1;
+
+class Producer extends Thread
+{
+	//Producer Producing the data in Stringbuffer
+	StringBuffer sB;
+	
+	//variable 
+	boolean dataProvider = false;
+	@Override
+	public void run() {
+
+		for (int i = 0; i <= 10; i++) {
+			sB.append(i+ " : ");
+			try 
+			{
+				Thread.sleep(100);
+				System.out.println("appending");
+			} 
+			catch (InterruptedException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+////////////////////////////////////////////////////
+class Consumer extends Thread
+{
+	Producer producer;
+	
+	public Consumer(Producer producer) {
+		super();
+		this.producer = producer;
+	}
+
+
+	@Override
+	public void run() {
+		while (producer.dataProvider == false) {
+			
+			try 
+			{
+				Thread.sleep(10);
+			} 
+			catch (InterruptedException e) 
+			{
+				
+				e.printStackTrace();
+			}
+		}
+		
+		//Consumer the Data
+		System.out.println(producer.sB);
+	}
+}
+
+
+public class Communication {
+
+	public static void main(String[] args) 
+	{
+		Producer obj1 = new Producer();
+		Consumer obj2 = new Consumer(obj1);
+
+	}
+
+}
+
+
+1:47
