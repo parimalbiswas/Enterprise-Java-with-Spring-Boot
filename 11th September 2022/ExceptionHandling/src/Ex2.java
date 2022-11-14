5cbe0f10ff2079ed4585a069e2954bf44c609f25
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-11-14 20:25:21 +0530

    ...

--- 11th September 2022/ExceptionHandling/src/Ex2.java
+++ 11th September 2022/ExceptionHandling/src/Ex2.java
@@ -7,7 +7,7 @@ public class Ex2
 	public static void main(String[] args)
 	{
 		System.out.println("createing array below");
-//		int a[] = new int[9999999999];  // continious Memory 0 to 999999 not free in memeory   
+//		int a[] = new int[9999999999];  // continuous Memory 0 to 999999 not free in memeory   
 
 	}
 
